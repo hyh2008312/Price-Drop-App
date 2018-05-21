86665279f5baac08dd6632b1396da2c9012633e2
hyh2008312 <hyh2008312@163.com>
2018-05-21 10:48:25 +0800

    [Pages]:[TopChannel]:correct the top channel

--- src/js/pages/mobile/home/topChannel.vue
+++ src/js/pages/mobile/home/topChannel.vue
@@ -5,58 +5,58 @@
             <text class="i-c" v-for="(item,index) in channelList"
                   :class="[ activeIndex == index ? 'c-act' : '']"
                   @click="chooseChannel(index,item)" :ref="'channel_' + index"
-                :style="{width: (item.width + 48) + 'px'}">{{item.name}}</text>
+                  :style="{width: (item.width + 48) + 'px'}">{{item.name}}</text>
         </scroller>
     </div>
 </template>
 <script>
-const dom = weex.requireModule('dom')
-const animation = weex.requireModule('animation')
-export default {
-    props: {
-        activeIndex: {
-            type: Number,
-            default: 0
+    const dom = weex.requireModule('dom')
+    const animation = weex.requireModule('animation')
+    export default {
+        props: {
+            activeIndex: {
+                type: Number,
+                default: 0
+            },
+            channelList: {
+                type: Array,
+                default: []
+            }
         },
-        channelList: {
-            type: Array,
-            default: []
-        }
-    },
-    watch: {
-        activeIndex (newVal, oldVal) {
-            const scrollIndex = newVal < 4 ? 0 : newVal - 4;
-            this.changeRect(newVal)
-            this.scrollToMenu(scrollIndex)
-        }
-    },
-    created () {},
-    data () {
-        return {
-            dataReady: false
-        }
-    },
-    methods: {
-        chooseChannel (index, channel) {
-            this.activeIndex = index
-            this.$emit('change', index)
+        watch: {
+            activeIndex (newVal, oldVal) {
+                const scrollIndex = newVal < 4 ? 0 : newVal - 4;
+                this.changeRect(newVal)
+                this.scrollToMenu(scrollIndex)
+            }
         },
-        changeRect (index) {
-            const item = this.channelList[index]
-            animation.transition(this.$refs.jcLine, {
-                styles: {
-                    width: item.width + 'px',
-                    transform: 'translateX(' + (item.left) + 'px)'
-                },
-                duration: 100,
-                timingFunction: 'ease',
-                delay: 0
-            }, function () {});
+        created () {},
+        data () {
+            return {
+                dataReady: false
+            }
         },
-        scrollToMenu (index) {
-            dom.scrollToElement(this.$refs[`channel_${index}`][0])
+        methods: {
+            chooseChannel (index, channel) {
+                this.activeIndex = index
+                this.$emit('change', index)
+            },
+            changeRect (index) {
+                const item = this.channelList[index]
+                animation.transition(this.$refs.jcLine, {
+                    styles: {
+                        width: item.width + 'px',
+                        transform: 'translateX(' + (item.left) + 'px)'
+                    },
+                    duration: 100,
+                    timingFunction: 'ease',
+                    delay: 0
+                }, function () {});
+            },
+            scrollToMenu (index) {
+                dom.scrollToElement(this.$refs[`channel_${index}`][0])
+            }
         }
     }
-}
 </script>
 <style lang="sass" src="./topChannel.scss"></style>
