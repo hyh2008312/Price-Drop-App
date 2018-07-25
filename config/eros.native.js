084637214796b7cd5c330f8f0027b02872339ceb
hyh2008312 <hyh2008312@163.com>
2018-07-25 20:00:22 +0800

    [Config]:[Eros.Native]:correct the eros config

--- config/eros.native.js
+++ config/eros.native.js
@@ -4,7 +4,7 @@ module.exports = {
     // android 监听全局事件homeBack 如果为true 安卓端需要自行调用router.finish方法来关闭应用
     'androidIsListenHomeBack': 'true',
     'version': {
-        'android': '1.3.3'
+        'android': '1.3.4'
     },
     'page': {
         'homePage': '/pages/mobile/welcome/guide.js',
