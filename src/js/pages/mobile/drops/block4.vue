a4369d5a27d47e31bdf8d7243c7deb9a093d5ce3
hyh2008312 <hyh2008312@163.com>
2018-08-22 15:22:42 +0800

    [Home]:[Block4]:correct the home block4

--- src/js/pages/mobile/drops/block4.vue
+++ src/js/pages/mobile/drops/block4.vue
@@ -5,7 +5,7 @@
                 <div class="header">
                     <preload class="header-image" :src="message.avatar"></preload>
                 </div>
-                <text class="tlt">{{message.username}} {{message.title}} </text>
+                <text class="tlt">{{message.title}} </text>
             </div>
         </div>
     </div>
