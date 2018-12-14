6f5fbffb58efcd6846daa9ba3a8204893c4ff9b4
hyh2008312 <hyh2008312@163.com>
2018-12-14 12:35:57 +0800

    [Warehouse]:[Tracking]:add warehouse tracking pages

---
+++ src/js/pages/mobile/common/notification.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="wrapper">
+        <div ref="notice">
+            <div class="inner-container" v-for="message in items">
+                <text class="icon iconfont">&#xe760;</text>
+                <text class="tips">Free Shipping on Everything. Limited Time Only! </text>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    const animation = weex.requireModule('animation');
+    import utilFunc from '../utils/util'
+
+    export default {
+        props: ['items', 'index', 'activeIndex'],
+        data () {
+            return {
+                isOnAni: false
+            }
+        },
+        mounted () {
+            this.init()
+        },
+        watch: {
+            items (newVal, oldVal) {
+                this.resetAni()
+            }
+        },
+        methods: {
+            init () {
+                const index = -1;
+                this.aniNotice(index)
+            },
+            aniNotice (e) {
+                const that = this;
+                if (!this.isOnAni) {
+                    this.isOnAni = true;
+                    animation.transition(this.$refs.notice, {
+                        styles: {
+                            opacity: 1,
+                            transform: 'translateY(' + (-64 * (e + 1)) + 'px)'
+                        },
+                        duration: 500,
+                        timingFunction: 'ease',
+                        needLayout: true,
+                        delay: 3500
+                    }, function () {
+                        that.isOnAni = false;
+                        if (e < that.items.length - 1) {
+                            e++;
+                            if (e == that.items.length - 1) {
+                                e = -1;
+                                // that.$emit('noticeFinished', {
+                                //     status: 'finished'
+                                // });
+                                // return
+                            }
+                            that.aniNotice(e)
+                        }
+                    });
+                }
+            },
+            resetAni (e) {
+                const that = this;
+                animation.transition(this.$refs.notice, {
+                    styles: {
+                        opacity: 0,
+                        transform: 'translateY(0px)'
+                    },
+                    duration: 0,
+                    timingFunction: 'ease',
+                    delay: 200
+                }, function () {
+                    that.isOnAni = false;
+                    that.init()
+                })
+            },
+            formateDate (dateTimeStamp) {
+                return utilFunc.formateDate(dateTimeStamp);
+            }
+        }
+    }
+</script>
+<style scoped>
+    .iconfont {
+        font-family: iconfont;
+    }
+
+    .wrapper{
+        background-color: #FFE3B6;
+        box-shadow: 0 1px 1px 0 rgba(0,0,0,0.12);
+        border-radius: 32px;
+        width: 750px;
+        height: 64px;
+    }
+
+    .inner-container{
+        width: 686px;
+        height: 64px;
+        justify-content: center;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .tips{
+        font-family: ProximaNova;
+        font-size: 24px;
+        line-height: 28px;
+        margin-left: 14px;
+        font-weight: 700;
+    }
+
+    .icon{
+        font-family: ProximaNova;
+        font-size: 32px;
+        line-height: 32px;
+        color: #EF8A31;
+    }
+</style>
