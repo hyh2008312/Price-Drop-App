8184c5e14d0e218c63b7b75ed9a20977295bc5f5
hyh2008312 <hyh2008312@163.com>
2019-02-14 12:52:59 +0800

    [Home]:[Config]:correct the config

--- src/js/pages/mobile/home/config.js
+++ src/js/pages/mobile/home/config.js
@@ -63,40 +63,12 @@ export const CHANNELLIST1 = [{
 }];
 
 export const PRODUCTS = [{
-    name: 'Electronics',
-    value: 'Electronic',
+    name: 'Kids',
+    value: 'Kids',
     width: 162,
-    color: '#f8b233',
-    src: 'http://image.getpricedrop.com/banner/20181029/electronics-2.png',
-    bgSrc: 'http://image.getpricedrop.com/banner/20181121/1.png'
-}, {
-    name: 'Men',
-    value: 'Men',
-    width: 163,
-    color: '#006de6',
-    src: 'http://image.getpricedrop.com/banner/20181029/men.png',
-    bgSrc: 'http://image.getpricedrop.com/banner/20181121/2.png'
-}, {
-    name: 'Women',
-    value: 'Women',
-    width: 162,
-    color: '#e16280',
-    src: 'http://image.getpricedrop.com/banner/20181121/women.png',
-    bgSrc: 'http://image.getpricedrop.com/banner/20181121/bn-women.png'
-}, {
-    name: 'Men\'s Shoes',
-    value: 'Shoes',
-    width: 163,
-    color: '#00ACEC',
-    src: 'http://image.getpricedrop.com/banner/20181121/m-shoes.png',
-    bgSrc: 'http://image.getpricedrop.com/banner/20181121/men-shoes.png'
-}, {
-    name: 'Women\'s Shoes',
-    value: 'Shoes',
-    width: 209,
-    color: '#f8932B',
-    src: 'http://image.getpricedrop.com/banner/20181121/w-shoes.png',
-    bgSrc: 'http://image.getpricedrop.com/banner/20181121/women-shoes.png'
+    color: '#F7D363',
+    src: 'http://image.getpricedrop.com/banner/20181121/kids-conner.png',
+    bgSrc: 'http://image.getpricedrop.com/banner/20181121/kids.png'
 }, {
     name: 'Bags',
     value: 'Bags',
@@ -105,13 +77,6 @@ export const PRODUCTS = [{
     src: 'http://image.getpricedrop.com/banner/20181029/bags-1.png',
     bgSrc: 'http://image.getpricedrop.com/banner/20181121/4.png'
 }, {
-    name: 'Jewellery & Watches',
-    value: 'Jewellery & Watches',
-    width: 163,
-    color: '#6822d3',
-    src: 'http://image.getpricedrop.com/banner/20181029/jewellery.png',
-    bgSrc: 'http://image.getpricedrop.com/banner/20181121/5.png'
-}, {
     name: 'Home',
     value: 'Home',
     width: 120,
@@ -119,17 +84,52 @@ export const PRODUCTS = [{
     src: 'http://image.getpricedrop.com/banner/20181029/home.png',
     bgSrc: 'http://image.getpricedrop.com/banner/20181121/3.png'
 }, {
+    name: 'Women',
+    value: 'Women',
+    width: 162,
+    color: '#e16280',
+    src: 'http://image.getpricedrop.com/banner/20181121/women.png',
+    bgSrc: 'http://image.getpricedrop.com/banner/20181121/bn-women.png'
+}, {
+    name: 'Men',
+    value: 'Men',
+    width: 163,
+    color: '#006de6',
+    src: 'http://image.getpricedrop.com/banner/20181029/men.png',
+    bgSrc: 'http://image.getpricedrop.com/banner/20181121/2.png'
+}, {
+    name: 'Women\'s Shoes',
+    value: 'Shoes',
+    width: 209,
+    color: '#f8932B',
+    src: 'http://image.getpricedrop.com/banner/20181121/w-shoes.png',
+    bgSrc: 'http://image.getpricedrop.com/banner/20181121/women-shoes.png'
+}, {
+    name: 'Men\'s Shoes',
+    value: 'Shoes',
+    width: 163,
+    color: '#00ACEC',
+    src: 'http://image.getpricedrop.com/banner/20181121/m-shoes.png',
+    bgSrc: 'http://image.getpricedrop.com/banner/20181121/men-shoes.png'
+}, {
+    name: 'Electronics',
+    value: 'Electronic',
+    width: 162,
+    color: '#f8b233',
+    src: 'http://image.getpricedrop.com/banner/20181029/electronics-2.png',
+    bgSrc: 'http://image.getpricedrop.com/banner/20181121/1.png'
+}, {
+    name: 'Jewellery & Watches',
+    value: 'Jewellery & Watches',
+    width: 163,
+    color: '#6822d3',
+    src: 'http://image.getpricedrop.com/banner/20181029/jewellery.png',
+    bgSrc: 'http://image.getpricedrop.com/banner/20181121/5.png'
+}, {
     name: 'Beauty & Personal Care',
     value: 'Beauty & Personal Care',
     width: 162,
     color: '#FA57D1',
     src: 'http://image.getpricedrop.com/banner/20181121/beauty.png',
     bgSrc: 'http://image.getpricedrop.com/banner/20181121/bn-beauty.png'
-}, {
-    name: 'Kids',
-    value: 'Kids',
-    width: 162,
-    color: '#F7D363',
-    src: 'http://image.getpricedrop.com/banner/20181121/kids-conner.png',
-    bgSrc: 'http://image.getpricedrop.com/banner/20181121/kids.png'
 }];
